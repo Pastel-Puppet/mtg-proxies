396b373bb79be651bfc02d352bd3ebb7c1b05391
Pastel-Puppet <31093073+Pastel-Puppet@users.noreply.github.com>
2025-07-01 01:22:52 +0000

    Single point of truth for base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import htmlMinifier from 'vite-plugin-html-minifier';
 
 export default defineConfig({
     root: "www",
-    base: "https://mtg-proxies.pastel-puppet.workers.dev/",
+    base: "https://mtg-proxies.pastel-puppet.workers.dev",
     build: {
         target: "esnext",
         assetsInlineLimit: 10240, 
